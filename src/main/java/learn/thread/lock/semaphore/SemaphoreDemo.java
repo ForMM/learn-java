d80ec17504f459ef640ff70bdf33f9132dd342c2
linkk-mac <linkeke_sea@163.com>
2020-11-03 08:24:04 +0800

    lock

---
+++ src/main/java/learn/thread/lock/semaphore/SemaphoreDemo.java
@@ -0,0 +1,33 @@
+package learn.thread.lock.semaphore;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Semaphore semaphore = new Semaphore(3);
+
+        Worker w1 = new Worker(semaphore,"张三");
+        Worker w2 = new Worker(semaphore,"李四");
+        Worker w3 = new Worker(semaphore,"王二");
+        Worker w4 = new Worker(semaphore,"赵六");
+        Worker w5 = new Worker(semaphore,"张杰");
+        Worker w6 = new Worker(semaphore,"王强");
+
+
+        executor.execute(w1);
+        executor.execute(w2);
+        executor.execute(w3);
+        executor.execute(w4);
+        executor.execute(w5);
+        executor.execute(w6);
+
+        executor.shutdown();
+
+
+    }
+}
---
+++ src/main/java/learn/thread/lock/semaphore/Worker.java
@@ -0,0 +1,35 @@
+package learn.thread.lock.semaphore;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Worker implements Runnable {
+    private Semaphore semaphore;
+    private String name;
+
+    public Worker(Semaphore semaphore, String name) {
+        this.semaphore = semaphore;
+        this.name = name;
+    }
+
+    public void run() {
+        try {
+            //干活之前获取令牌
+            this.semaphore.acquire();
+            this.doWork();
+            TimeUnit.SECONDS.sleep(new Random().nextInt(10));
+            System.out.println(this.name+",活干完了");
+
+            //释放令牌
+            this.semaphore.release();
+            System.out.println("当前可用令牌数量："+this.semaphore.availablePermits());
+        }catch (InterruptedException ie){
+
+        }
+    }
+
+    public void doWork(){
+        System.out.println(name+",正在干活");
+    }
+}
