567d269bd2f1dee7061c4abf5a8a7b60b7b12f62
林可珂 <linkk@fadada.com>
2020-12-03 12:30:09 +0800

    cs

---
+++ src/main/java/learn/blockingQueue/Worker.java
@@ -0,0 +1,41 @@
+package learn.blockingQueue;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 有两个线程A,B, A线程每200ms就生成一个[0,100]之间的随机数， B线程每2S中打印出A线程所产生的增量随机数。
+ */
+public class Worker implements Runnable{
+	private volatile boolean isRunning = true;
+	private BlockingQueue blockingQueue;
+	private String name;
+	private static AtomicInteger count = new AtomicInteger();
+
+	public Worker(BlockingQueue blockingQueue, String name) {
+		this.blockingQueue = blockingQueue;
+		this.name = name;
+	}
+
+	public void run() {
+		System.out.println("启动生产者线程："+this.name);
+		Random random = new Random();
+		try {
+			while (isRunning){
+
+			}
+		}catch (Exception e){
+
+		}finally {
+			System.out.println("退出生产者线程："+this.name);
+		}
+
+
+	}
+
+	public void stop(){
+		isRunning = false;
+	}
+
+}
---
+++ src/main/java/learn/thread/pool/SingletonThreadPool.java
@@ -0,0 +1,33 @@
+package learn.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SingletonThreadPool {
+
+    private static int corePoolSize=10;
+    private static int maxPoolSize=100;
+    private static int keepAliveTime=100;
+    private static int blockingQueueSize=1000;
+
+    private ExecutorService executorService;
+
+    private SingletonThreadPool(){
+        executorService = new ThreadPoolExecutor(corePoolSize,maxPoolSize,keepAliveTime,TimeUnit.MICROSECONDS,new LinkedBlockingQueue<Runnable>(blockingQueueSize));
+    }
+
+    private static class SingletonContainer{
+        private static SingletonThreadPool instance = new SingletonThreadPool();
+    }
+
+    public static SingletonThreadPool getInstance(){
+        return SingletonContainer.instance;
+    }
+
+    public ExecutorService getThreadPool(){
+        return executorService;
+    }
+
+}
